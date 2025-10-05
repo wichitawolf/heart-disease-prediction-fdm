cdf3907108f97ac262fdc8b5b38fe1f91f40a083
Amararathne DPIN <90952893+wichitawolf@users.noreply.github.com>
2025-10-05 21:39:51 +0530

    Update urls.py

--- config/health_desease/urls.py
+++ config/health_desease/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from core.health.views import *
+from apps.core.health.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', add_heartdetail, name="home"),
