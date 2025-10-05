c87c98e9656962e734cc6d3692189a73c672020e
Amararathne DPIN <90952893+wichitawolf@users.noreply.github.com>
2025-10-05 21:25:03 +0530

    Update app name in HealthConfig

--- apps/core/health/apps.py
+++ apps/core/health/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class HealthConfig(AppConfig):
-    name = 'core.health'
+    name = 'apps.core.health'
     default_auto_field = 'django.db.models.BigAutoField'
