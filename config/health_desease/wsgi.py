604a700f6bd20d125e0c0e16fb69e82ab37a6474
Amararathne DPIN <90952893+wichitawolf@users.noreply.github.com>
2025-10-05 20:52:01 +0530

    Update DJANGO_SETTINGS_MODULE path in wsgi.py

--- config/health_desease/wsgi.py
+++ config/health_desease/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'health_desease.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.health_desease.settings')
 
 application = get_wsgi_application()
