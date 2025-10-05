9e2ef959a0a53e66f941fad6fd0845f279fed31f
Amararathne DPIN <90952893+wichitawolf@users.noreply.github.com>
2025-10-05 21:32:33 +0530

    Update ROOT_URLCONF to use full module path

--- config/health_desease/settings.py
+++ config/health_desease/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'health_desease.urls'
+ROOT_URLCONF = 'config.health_desease.urls'
 
 TEMPLATES = [
     {
